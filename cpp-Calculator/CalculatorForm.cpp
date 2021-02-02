0b14977c66399586bb46eaf2bf0d65b86b62d70f
Andrei <78364462+Two-Apes@users.noreply.github.com>
2021-02-02 01:22:24 -1000

    Add files via upload

---
+++ cpp-Calculator/CalculatorForm.cpp
@@ -0,0 +1,13 @@
+#include "CalculatorForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+void Main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	cppCalculator::CalculatorForm form;
+	Application::Run(% form);
+}
+
